d57b251c0b47bb3458d522dda994b2fe2b4ffe19
jaewon <jaewon990905@gmail.com>
2026-08-21 17:52:45 +0900

    스프라이트 파서 진행중

---
+++ CraftEngine/Resource/PixelSprite.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include <Core/Core.h>
+#include <Math/Color.h>
+
+#include <vector>
+
+namespace Craft
+{
+    struct CRAFT_API PixelSprite
+    {
+        int Width;
+        int Height;
+        std::vector<Craft::Color> Pixels;
+    };
+}
---
+++ CraftEngine/Resource/TextSpriteLoader.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include <Core/Core.h>
+#include <Resource/PixelSprite.h>
+
+#include <string>
+
+namespace Craft
+{
+    class CRAFT_API TextSpriteLoader
+    {
+    public:
+        static PixelSprite Load(const std::string& path);
+    };
+}
