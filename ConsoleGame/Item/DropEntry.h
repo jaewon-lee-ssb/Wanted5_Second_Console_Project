24a719b22ad2d805c03c8cb1b75f22d9e68ebdf1
jaewon <jaewon990905@gmail.com>
2026-09-07 18:27:21 +0900

    드랍아이템 제작중 저장

---
+++ ConsoleGame/Item/DropEntry.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include <Item/ItemData.h>
+
+#include <memory>
+
+struct DropEntry
+{
+	std::shared_ptr<const ItemData> itemData;
+
+	int weight = 0;
+};
---
+++ ConsoleGame/Item/ItemPickup.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include <Actor/Actor.h>
+
+class ItemData;
+
+class ItemPickup : public Craft::Actor
+{
+	TYPE_DECLARATIONS(ItemPickup, Actor)
+
+public:
+	ItemPickup(const Craft::Vector2F& position);
+	~ItemPickup() = default;
+
+private:
+
+private:
+	std::weak_ptr<ItemData> itemData;
+
+
+};
+
