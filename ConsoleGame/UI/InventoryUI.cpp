9af267b8d7ba7910c777a4352b07b03a02f6492e
jaewon <jaewon990905@gmail.com>
2026-09-07 09:17:15 +0900

    InventoryUI 구현

---
+++ ConsoleGame/UI/InventoryUI.cpp
@@ -0,0 +1,60 @@
+﻿#include <UI/InventoryUI.h>
+
+#include <Inventory/Inventory.h>
+
+#include <Render/Renderer.h>
+#include <Math/Color.h>
+
+InventoryUI::InventoryUI(const std::shared_ptr<const Inventory>& inventory, const Craft::Vector2F& position)
+	: inventory(inventory), position(position)
+{
+	CreateSlotImage();
+}
+
+void InventoryUI::Tick(float deltaTime)
+{
+	
+}
+
+void InventoryUI::Draw()
+{
+	auto lockedInventory = inventory.lock();
+
+	if (!lockedInventory)
+	{
+		return;
+	}
+
+	const int columnCount = lockedInventory->GetColumnCount();
+	const int rowCount = lockedInventory->GetRowCount();
+	
+	for (int y = 0; y < rowCount; ++y)
+	{
+		for (int x = 0; x < columnCount; ++x)
+		{
+			Craft::Vector2F slotPosition(position.x + x * slotWidth, position.y + y * slotHeight);
+
+			Craft::Renderer::Get().SubmitUI(slotImage, slotPosition);
+		}
+	}
+}
+
+void InventoryUI::CreateSlotImage()
+{
+	slotImage.width = slotWidth;
+	slotImage.height = slotHeight;
+	slotImage.pixels.resize(slotWidth * slotHeight);
+
+	for (int y = 0; y < slotHeight; ++y)
+	{
+		for (int x = 0; x < slotWidth; ++x)
+		{
+			Craft::Pixel& pixel = slotImage.At(x, y);
+
+			const bool isBorder = x == 0 || x == slotWidth - 1 || y == 0 || y == slotHeight - 1;
+
+			pixel.transparent = false;
+			pixel.color = isBorder ? Craft::BackgroundColor::LightGray : Craft::BackgroundColor::Black;
+		}
+	}
+}
---
+++ ConsoleGame/UI/InventoryUI.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+
+#include <UIManager/UIElement.h>
+#include <Math/Vector2.h>
+#include <Resource/PixelImage.h>
+
+#include <memory>
+
+class Inventory;
+
+class InventoryUI : public Craft::UIElement
+{
+public:
+	InventoryUI(const std::shared_ptr<const Inventory>& inventory, const Craft::Vector2F& position);
+
+	virtual ~InventoryUI() = default;
+
+	virtual void Tick(float deltaTime) override;
+	virtual void Draw() override;
+
+private:
+	void CreateSlotImage();
+
+private:
+	// 화면에 표시할 인벤토리를 관찰한다.
+	std::weak_ptr<const Inventory> inventory;
+
+	// 카메라 영향을 받지 않는 화면 좌표이다.
+	Craft::Vector2F position;
+
+	Craft::PixelImage slotImage;
+
+	static constexpr int slotWidth = 6;
+	static constexpr int slotHeight = 3;
+};
+
